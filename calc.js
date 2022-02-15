a0cd34867be5a9e31cb7a22422ffb2673379ab03
Leonardo Lopes <96753634+leobraboo@users.noreply.github.com>
2022-02-14 22:36:23 -0300

    Delete calc.js

--- calc.js
+++
@@ -1,9 +0,0 @@
-var somafunc = require("./soma");
-var subfunc = require("./sub");
-var divfunc = require("./div");
-var multfunc = require("./mult");
-
-console.log(somafunc(50,1));
-console.log(subfunc(545,145));
-console.log(divfunc(56,13));
-console.log(multfunc(53,10));
